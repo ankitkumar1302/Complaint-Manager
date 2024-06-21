5d949cfdc22ee1afafe4a819bce7adae01a789ab
Ankit <ankit_1302@outlook.com>
2024-06-21 18:26:53 +0530

    Initial Commit

---
+++ src/test/java/com/example/complaint_manager/ComplaintManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.complaint_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
